b1ddf8f581a5ffa6c44bb0aaaae65c8a5bfdb2cf
SariIlhami <144421525+SariIlhami@users.noreply.github.com>
2023-09-22 06:22:25 +0700

    Add files via upload

---
+++ Tugas Prak Fiskom Sari.py
@@ -0,0 +1,15 @@
+# Menentukan variabel
+F = 750  # Gaya dalam newton
+h = 15   # Ketinggian dalam meter
+t = 20   # Waktu dalam detik
+
+# Menghitung Kecepatan Motor
+v = h / t
+
+# Menghitung kerja yang dilakukan
+W = F * h
+
+# Menghitung daya
+P = W / t
+
+print("Daya minimum yang harus disediakan oleh motor:", P, "Watt")
